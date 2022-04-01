1729e225f18b7d55c6eac80acc3573cd87d168ee
JessicaLima <jessikurt@gmail.com>
2022-04-01 01:28:39 -0300

    feat: created Header component

---
+++ src/components/Header/styles.js
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+import media from "styled-media-query";
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: ${({ isActive }) => (isActive ? "center" : "flex-end")};
+`;
+
+export const List = styled.div`
+  display: ${({ isActive }) => (isActive ? "flex" : "none")};
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+`;
+
+export const ListItem = styled.div`
+  color: #ada8b6;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid #ada8b6;
+  padding: 0.5rem 0;
+  font-size: x-small;
+  span {
+    color: yellowgreen;
+    padding: 0 0.2rem 0 0;
+  }
+  svg {
+    font-size: small;
+  }
+  &:last-child {
+    border: none;
+  }
+  ${media.between("medium", "large")`
+    padding: 1rem 1.2rem;
+    font-size: small;
+    svg {
+    font-size: large;
+    }
+  ${media.greaterThan("large")`
+    padding: 3rem 1.2rem;
+    font-size: small;
+    svg {
+    font-size: large;
+    }
+  `}
+  `}
+`;
+
+export const Button = styled.button`
+  display: ${({ isActive }) => (isActive ? "none" : "flex")};
+  background: transparent;
+  border: none;
+  color: #ada8b6;
+  padding: 0.2rem 0.5rem;
+  svg {
+    font-size: 2rem;
+  }
+  ${media.between("medium", "large")`
+    padding: 1rem 1.2rem;
+  `}
+`;
