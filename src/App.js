2fc3651513b2f634ff8f471fc93d0661d4f38a50
JessicaLima <jessikurt@gmail.com>
2022-04-01 01:28:07 -0300

    feat: create react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import HomePage from './pages/HomePage'
+import Global from './style/global';
+
+function App() {
+  return (
+    <div>
+      <Global />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
