91eddc22fdc783d14d43ab0e7a3e2624dde09148
JessicaLima <jessikurt@gmail.com>
2022-04-06 19:47:21 -0300

    feat: add route for portfolio

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from '@mui/icons-material/Close';
 import { Container, List, ListItem, Button } from "./styles";
 
 const Header = () => {
   const [isActive, setIsActive] = useState(false);
-
+  const navigate = useNavigate()
   console.log(isActive);
   return (
     <Container isActive={isActive}>
@@ -16,7 +18,7 @@ const Header = () => {
       </Button>
       <List isActive={isActive}>
         <ListItem><span>&#62;</span> Home</ListItem>
-        <ListItem>Portfolio</ListItem>
+        <ListItem onClick={()=>navigate("/portfolio")}>Portfolio</ListItem>
         <ListItem>Curriculo</ListItem>
         <ListItem>Contato</ListItem>
         <ListItem onClick={() => setIsActive(false)}><CloseIcon /></ListItem>
