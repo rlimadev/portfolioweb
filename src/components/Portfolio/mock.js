5c6365c33d6a5e415cdefb77e6a96a16549552eb
JessicaLima <jessikurt@gmail.com>
2022-04-29 14:25:33 -0300

    feat: included url for project

--- src/components/Portfolio/mock.js
+++ src/components/Portfolio/mock.js
@@ -1,35 +1,52 @@
-import Brastemp from '../../assets/img/br1.jpg'
-import Buraco from '../../assets/img/bura2.jpg'
-import PhotoLivro from '../../assets/img/nicefoto1.jpg'
-import Medsoft from '../../assets/img/med1.jpg'
-import SwordUnknown from '../../assets/img/swor1.jpg'
-import Belote from '../../assets/img/belo1.jpg'
-
+import Brastemp from "../../assets/img/br1.jpg";
+import Buraco from "../../assets/img/bura2.jpg";
+import PhotoLivro from "../../assets/img/nicefoto1.jpg";
+import Medsoft from "../../assets/img/med1.jpg";
+import SwordUnknown from "../../assets/img/swor1.jpg";
+import Belote from "../../assets/img/belo1.jpg";
+import Microlins from "../../assets/img/lins2mini.jpg";
 
 export const projects = [
   {
     title: "Aplicativo Brastemp",
     subtitle: "Programação",
-    image: Brastemp
-  },{
+    url: "brastemp",
+    image: Brastemp,
+  },
+  {
     title: "Jogo Buraco Aberto",
     subtitle: "Programação",
-    image: Buraco
-  },{
+    url: "buraco",
+    image: Buraco,
+  },
+  {
     title: "Web App Photo Livro",
     subtitle: "Programação",
-    image: PhotoLivro
-  },{
+    url: "photolivro",
+    image: PhotoLivro,
+  },
+  {
     title: "Aplicativo Medsoft",
     subtitle: "Programação",
-    image: Medsoft
-  },{
+    url: "medsoft",
+    image: Medsoft,
+  },
+  {
+    title: "Aplicativo Microlins",
+    subtitle: "Programação",
+    url: "microlins",
+    image: Microlins,
+  },
+  {
     title: "Jogo SWORD OF UNKNOWN",
     subtitle: "Programação",
-    image: SwordUnknown
-  },{
+    url: "sword",
+    image: SwordUnknown,
+  },
+  {
     title: "Jogo Belote",
     subtitle: "Programação",
-    image: Belote
+    url: "belote",
+    image: Belote,
   },
-]
+];
