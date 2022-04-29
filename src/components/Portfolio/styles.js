13a47fee8ccb9bbd5856b4305abf3a59df99c069
JessicaLima <jessikurt@gmail.com>
2022-04-29 14:26:35 -0300

    styles: included a section

--- src/components/Portfolio/styles.js
+++ src/components/Portfolio/styles.js
@@ -1,36 +1,41 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  //background: red;
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
+`;
+
+export const Section = styled.section`
+  width: 60%;
 `;
 
 export const Title = styled.div`
   color: yellowgreen;
   width: 90%;
-  font-size: 7.4rem;
+  font-size: 4.1rem;
   font-weight: bold;
 `;
 
 export const Nav = styled.nav`
-  width: 90%;
+  width: 100%;
+  font-size: 1.5rem;
+  margin: 0 0 2.7rem 0;
 `;
 
 export const List = styled.ul`
   background-repeat: no-repeat;
-  width: 90%;
+  width: 87%;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
-  grid-gap: 4.5rem;
-  margin: 2rem 0 8rem 0;
+  grid-template-rows: 320px 320px 320px;
+  grid-gap: 2rem;
+  margin: 3rem 0 8rem 0;
 `;
 
 export const Content = styled.div`
   z-index: 99999;
-  //
-  //background-opacity: .5;
   border-radius: 10px;
   width: 100%;
   height: 500px;
@@ -39,28 +44,30 @@ export const Content = styled.div`
 export const ListItem = styled.li`
   cursor: pointer;
   width: 100%;
-  height: 500px;
-  border-radius: 10px;
+  height: 320px;
   background-size: cover;
   div {
     width: 100%;
     height: 100%;
+    opacity: 0;
     background: rgba(0, 0, 0, 0.9);
-    border-radius: 10px;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    &:hover {
+      opacity: 1;
+    }
   }
   h2 {
     color: yellowgreen;
-    font-size: 2.3rem;
+    font-size: fit-content;
     text-transform: uppercase;
     opacity: 1;
   }
-  span{
+  span {
     text-transform: uppercase;
-    font-size: x-large;
+    font-size: fit-content;
     font-weight: bold;
   }
 `;
