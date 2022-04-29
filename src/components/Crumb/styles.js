9704044f4e8ac57f8f46c2dfc8fe943ae9c66158
JessicaLima <jessikurt@gmail.com>
2022-04-29 14:24:28 -0300

    styles: included style for button

--- src/components/Crumb/styles.js
+++ src/components/Crumb/styles.js
@@ -1,7 +1,14 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-  font-size: 2.6rem;
-  margin: 0 0 4rem 0rem;
+  width: 100%;
 `;
 
+export const Btn = styled.button`
+  background: none;
+  border: none;
+  color: #ada8b6;
+  margin: 2rem 0.4rem 0rem 0;
+  font-size: 2.3rem;
+  letter-spacing: 0.04rem;
+`;
